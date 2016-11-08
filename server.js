d7de10f25c08035e672bb38c0faa0f9655af0725
alexisgraff5 <lex.graff@gmail.com>
2016-11-08 14:40:28 -0700

    launched the Angular client

--- server.js
+++ server.js
@@ -30,7 +30,21 @@ app.post('/', function( req, res ) {
     'X-XSS-Protection': '1; mode=block',
     'X-Frame-Options': 'SAMEORIGIN',
     'Content-Security-Policy': "default-src 'self' devmountain.github.io"
-  }).send(messages);  
+  }).send(messages);
+});
+
+app.options('/', function( req, res ) {
+  messages.push(req.body.message);
+
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(messages);
 });
 
 app.listen(8000, function() {
