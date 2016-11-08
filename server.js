8f747928130942148ae632d61709928e80bbec0f
alexisgraff5 <lex.graff@gmail.com>
2016-11-08 14:27:39 -0700

    created the server (get)

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+var express = require('express');
+var app = express();
 
+var messages = [];
+
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(messages);
+});
+
+app.listen(8000, function() {
+  console.log('listening!');
+});
