c33b225826a0186aeb585877ce0d368bf7bcdbcb
alexisgraff5 <lex.graff@gmail.com>
2016-11-08 14:35:14 -0700

    created POST part of server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var messages = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function( req, res ) {
   res.status(200).set({
@@ -13,6 +16,21 @@ app.get('/', function( req, res ) {
     'X-Frame-Options': 'SAMEORIGIN',
     'Content-Security-Policy': "default-src 'self' devmountain.github.io"
   }).send(messages);
+});
+
+app.post('/', function( req, res ) {
+  //console.log(req.body);
+  messages.push(req.body.message);
+
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(messages);  
 });
 
 app.listen(8000, function() {
